bcd5b5cc34e93a3826ccf1549f80ee9546e3702e
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-06-24 23:29:26 +0000

    Add logic for admins to edit current user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,13 +19,24 @@ class Admin::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       redirect_to admin_users_path, notice: "New User, #{@user.firstname} created!"
     else
       render :new
     end
+  end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path, notice: "User edit for, #{@user.firstname} has been completed!"
+    else
+      render :edit
+    end
   end
 
   protected
